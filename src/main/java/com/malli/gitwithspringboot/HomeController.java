79e3a1c212eb3a658301a6bbcab671607e92daea
nagarjunareddy1999 <nimmakayalanagarjunareddy19@gmail.com>
2023-08-18 17:23:29 +0530

    added Homecontroller

---
+++ src/main/java/com/malli/gitwithspringboot/HomeController.java
@@ -0,0 +1,12 @@
+package com.malli.gitwithspringboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String welcome() {
+        return "Welcome to Git with Spring Boot";
+    }
+}
