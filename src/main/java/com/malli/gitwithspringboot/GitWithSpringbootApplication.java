d21d002fab1cc34a10b0dcfe913fd636f655b609
nagarjunareddy1999 <nimmakayalanagarjunareddy19@gmail.com>
2023-08-18 15:51:49 +0530

    initial commit

---
+++ src/main/java/com/malli/gitwithspringboot/GitWithSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.malli.gitwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWithSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWithSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malli/gitwithspringboot/GitWithSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malli.gitwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWithSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
